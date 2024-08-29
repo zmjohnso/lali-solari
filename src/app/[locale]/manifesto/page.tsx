8e4ea1301b8b1ef89092c727fccb47389480d1bc
Zachary Johnson <zachary.business@tutanota.com>
2024-08-28 21:39:26 -0400

    update manifesto page photo

--- src/app/[locale]/manifesto/page.tsx
+++ src/app/[locale]/manifesto/page.tsx
@@ -1,5 +1,6 @@
 import { getManifestoPageData } from "@/lib/api";
 import BackButton from "@/src/components/backButton";
+import getBase64 from "@/src/shared/getBase64";
 import { getContentfulLocale } from "@/src/shared/utilities";
 import { unstable_setRequestLocale } from "next-intl/server";
 import Image from "next/image";
@@ -20,22 +21,27 @@ export default async function Manifesto({
     return <div>Something went wrong. Please try again later.</div>;
   }
 
+  const base64 = await getBase64(manifestoPage.media.url);
+
   return (
     <div className="px-4 md:px-40 pt-2 md:pt-20">
       <div className="mb-20">
         <h3 className="mb-8 font-bold text-[clamp(1rem,10vw,3rem)] font-open-sans uppercase">
           {manifestoPage.title}
         </h3>
-        <p className="flex self-center mb-8 w-80 md:w-7/12 ml-4 md:ml-52 font-arimo">
-          {manifestoPage.description}
-        </p>
         <Image
+          className="mb-8"
           src={manifestoPage.media.url}
           alt={manifestoPage.media.title}
           width={4032}
           height={2268}
+          placeholder="blur"
+          blurDataURL={base64}
           priority
         />
+        <p className="flex self-center mb-8 w-80 md:w-7/12 ml-4 md:ml-52 font-arimo">
+          {manifestoPage.description}
+        </p>
         <BackButton />
       </div>
     </div>
